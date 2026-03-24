a931ff37be14a93c371883095610a5be30b5b7a4
Julia Tallarico <julia.tallcaroline@outlook.com>
2026-03-23 23:12:18 -0300

    k

--- pb/tp3/src/main/java/org/example/banco/exception/ContaNaoEncontradaException.java
+++
@@ -1,8 +0,0 @@
-package org.example.banco.exception;
-
-public class ContaNaoEncontradaException extends RuntimeException {
-
-    public ContaNaoEncontradaException(Long id) {
-        super("Conta com id " + id + " não encontrada");
-    }
-}
--- pb/tp3/src/main/java/org/example/banco/exception/SaldoInvalidoException.java
+++
@@ -1,8 +0,0 @@
-package org.example.banco.exception;
-
-public class SaldoInvalidoException extends RuntimeException {
-
-    public SaldoInvalidoException(String msg) {
-        super(msg);
-    }
-}
